c2d865105d3fc8efd4492c78fabb5ba613f84910
taras <taras19041@gmail.com>
2020-02-17 19:12:53 +0200

    Add entity Material Add test

--- src/test/java/TestHibernateService.java
+++ src/test/java/TestHibernateService.java
@@ -1,5 +1,7 @@
+import entity.Material;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.junit.jupiter.api.Test;
 import services.HibernateService;
 
@@ -9,6 +11,18 @@ public class TestHibernateService {
     public void testSesionBuilder(){
         SessionFactory sessionFactory = HibernateService.getSessionFactory();
         Session session = sessionFactory.openSession();
+        session.close();
+    }
+
+    @Test
+    public void testMaterials(){
+        Session session = HibernateService.getSessionFactory().openSession();
+        session.beginTransaction();
+        Material material =new Material((long) 1, "Carbon");
+        System.out.println(material.toString());
+        session.save(material);
+        session.delete(material);
+        session.getTransaction().commit();
         session.close();
     }
 
